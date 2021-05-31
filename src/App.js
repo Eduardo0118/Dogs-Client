0d2014c4d085b0d7232329866e217a7146a88dec
Eduardo0118 <pro-cs-4@hotmail.com>
2021-05-31 13:44:25 -0300

    Initial login components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import './Global/Global.css';
+import 'Global/Global.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './Components/Footer/Footer';
-import Header from './Components/Header/Header';
-import Home from './Components/Home/Home';
-import Login from './Components/Login/Login';
+import Footer from 'Components/Footer/Footer';
+import Header from 'Components/Header/Header';
+import Home from 'Components/Home/Home';
+import Login from 'Components/Login/Login';
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login/*" element={<Login />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
+import LoginCreate from './LoginCreate';
+import LoginForgot from './LoginForgot';
+import LoginForm from './LoginForm';
+import LoginRecovery from './LoginRecovery';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<LoginForm />} />
+        <Route path="create" element={<LoginCreate />} />
+        <Route path="forgot-pass" element={<LoginForgot />} />
+        <Route path="recovery-pass" element={<LoginRecovery />} />
+      </Routes>
+    </>
+  );
 };
 
 export default Login;
