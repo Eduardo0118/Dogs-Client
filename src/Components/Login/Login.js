32ece53cd942c1945484641e4fb7eccdd58fa3ac
Eduardo <pro-cs-4@hotmail.com>
2021-06-02 20:47:21 -0300

    Responsive login layout

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,20 +5,23 @@ import LoginCreate from './LoginCreate';
 import LoginForgot from './LoginForgot';
 import LoginForm from './LoginForm';
 import LoginRecovery from './LoginRecovery';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login) return <Navigate to="/account" />;
   return (
-    <>
-      <Routes>
-        <Route path="" element={<LoginForm />} />
-        <Route path="create" element={<LoginCreate />} />
-        <Route path="forgot-pass" element={<LoginForgot />} />
-        <Route path="recovery-pass" element={<LoginRecovery />} />
-      </Routes>
-    </>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="" element={<LoginForm />} />
+          <Route path="create" element={<LoginCreate />} />
+          <Route path="forgot-pass" element={<LoginForgot />} />
+          <Route path="recovery-pass" element={<LoginRecovery />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
